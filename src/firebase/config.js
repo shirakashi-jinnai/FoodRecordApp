eaaf0481ce58f6a6918d4eec587950d966cc31ec
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2021-12-01 18:42:46 +0900

    環境変数を設定

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAEhbObHJJjdlM5WX03uNIXNqfXAa419qc",
-  authDomain: "cook-site.firebaseapp.com",
-  projectId: "cook-site",
-  storageBucket: "cook-site.appspot.com",
-  messagingSenderId: "320924907973",
-  appId: "1:320924907973:web:63da80d6f460cdf3fab3ac",
-  measurementId: "G-X0TNHE3QFE"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 export default firebaseConfig;
