eee085400af1a28284b937016a274b413f70503b
shirakashi-jinnai <j.shirakashi.jb@gmail.com>
2022-01-01 19:00:21 +0900

    不要なコードを削除

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import { ConnectedRouter } from "connected-react-router";
-import { createBrowserHistory } from "history";
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import teststore from "./reducks/store/store";
-// eslint-disable-next-line no-unused-vars
-import * as History from "history";
-import { theme } from "./asetts/theme";
-// eslint-disable-next-line no-unused-vars
-import { ThemeProvider } from "@material-ui/styles";
-import { MuiThemeProvider } from "@material-ui/core";
-import App from "./App";
+import { ConnectedRouter } from 'connected-react-router'
+import { createBrowserHistory } from 'history'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import teststore from './reducks/store/store'
+import { theme } from './asetts/theme'
+import { MuiThemeProvider } from '@material-ui/core'
+import App from './App'
 
-const history = createBrowserHistory();
-export const store = teststore(history);
+const history = createBrowserHistory()
+export const store = teststore(history)
 
 ReactDOM.render(
   <Provider store={store}>
@@ -23,5 +18,5 @@ ReactDOM.render(
       </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
